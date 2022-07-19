7f9affb1bdb3d8aa010c450c44d85b7e321ba855
guri-sachin <107688069+guri-sachin@users.noreply.github.com>
2022-07-19 12:13:00 +0530

    Add files via upload

---
+++ build/precache-manifest.4e2ffb79f6e6e96e185ace7d8a4fe81b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "903ac25d99122768334b",
+    "url": "/static/css/main.8a77f20f.chunk.css"
+  },
+  {
+    "revision": "903ac25d99122768334b",
+    "url": "/static/js/main.fe5e8245.chunk.js"
+  },
+  {
+    "revision": "e8492fdcaf2904e9aeed",
+    "url": "/static/js/runtime~main.8864b8e8.js"
+  },
+  {
+    "revision": "2bd2a76a222fe7c03c85",
+    "url": "/static/css/2.900b2186.chunk.css"
+  },
+  {
+    "revision": "2bd2a76a222fe7c03c85",
+    "url": "/static/js/2.74ac95d2.chunk.js"
+  },
+  {
+    "revision": "c6b1a2cb47ebd29a3dd7",
+    "url": "/static/js/3.6a1dbab2.chunk.js"
+  },
+  {
+    "revision": "db0a0272f68d438a4133cfceb510a2cd",
+    "url": "/index.html"
+  }
+];
